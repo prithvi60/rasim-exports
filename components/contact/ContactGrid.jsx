a6d672df1efc924f4ebbc2bcad9c20bb1905b09a
GOGA-23 <gokulgandhi97@gmail.com>
2024-10-07 18:49:41 +0530

    few changes

--- components/contact/ContactGrid.jsx
+++ components/contact/ContactGrid.jsx
@@ -31,11 +31,11 @@ const ContactGrid = () => {
                 </h4>
                 <ul className="w-full p-2 space-y-3 bg-primary sm:w-max">
                     <li>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d242.9247210789926!2d80.11232561629664!3d13.048682773950777!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a5261c39e8c0a61%3A0x10060ee946c2087!2sRasim%20Exports%20Private%20Limited!5e0!3m2!1sen!2sin!4v1727437573636!5m2!1sen!2sin" width={"100%"} height={"100%"} className="h-[80px]" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d242.9247210789926!2d80.11232561629664!3d13.048682773950777!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a5261c39e8c0a61%3A0x10060ee946c2087!2sRasim%20Exports%20Private%20Limited!5e0!3m2!1sen!2sin!4v1727437573636!5m2!1sen!2sin" width={"100%"} height={"100%"} className="h-[140px]" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                     </li>
-                    <li>
+                    {/* <li>
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d242.9247210789926!2d80.11232561629664!3d13.048682773950777!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a5261c39e8c0a61%3A0x10060ee946c2087!2sRasim%20Exports%20Private%20Limited!5e0!3m2!1sen!2sin!4v1727437573636!5m2!1sen!2sin" width={"100%"} height={"100%"} className="h-[80px]" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-                    </li>
+                    </li> */}
                 </ul>
             </div>
             <div className='block space-y-6 rounded-tl-[20px] bg-secondary p-5 '>
--- components/products/ProductHero.jsx
+++ components/products/ProductHero.jsx
@@ -20,7 +20,7 @@ const ProductHero = () => {
         <section className="relative w-full h-screen overflow-hidden slider-container">
             <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col sm:flex-row justify-center items-center gap-3 flex-wrap font-bold rounded-md py-2 px-3.5 z-10 w-full sm:w-3/5 lg:w-3/5">
                 <div className='block p-2 space-y-5 text-center rounded-tl-[20px] bg-primary w-full'>
-                    <h1 className='font-medium tracking-wide uppercase text-2xl md:text-4xl lg:text-[48px] font-libreCaslonDisplay text-secondary leading-8 lg:!leading-[50px]'>Discover Quality, Style, and Innovation in Every Piece.</h1>
+                    <h1 className='font-medium tracking-wide uppercase text-2xl md:text-4xl lg:text-[48px] font-libreCaslonDisplay text-secondary leading-8 lg:!leading-[50px]'>Discover Quality, Style, and Innovation in Every Piece</h1>
                     <div className='flex items-center justify-center gap-5 pb-3.5'>
                         <Link href={""} className='p-3 text-sm font-normal tracking-wide text-white uppercase bg-transparent border-2 md:text-lg md:border-4 border-secondary rounded-tl-xl font-figtree '>Get catalogue</Link>
                         <Link href={""} className='p-3 text-sm font-normal tracking-wide uppercase border-2 md:text-lg md:border-4 bg-secondary border-secondary rounded-tl-xl font-figtree '>View products</Link>
